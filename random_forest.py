7ee9699f0993e9050e8f096413f4865ab7623240
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-11-28 21:09:16 +0800

    Added the training/test classification report of other column combinations

--- random_forest.py
+++ random_forest.py
@@ -14,12 +14,6 @@ def classification_report_table(clf, x, y_true, labels, title):
     clf_report = classification_report(y_true, y_pred, labels=labels)
     return f"{title}\n{clf_report}"
 
-# Load data from pkl files
-x_train = read_data(filepath="x_train.pkl", istarget=False)
-y_train = read_data(filepath="y_train.pkl", istarget=True)
-x_test = read_data(filepath="x_test.pkl", istarget=False)
-y_test = read_data(filepath="y_test.pkl", istarget=True)
-
 # Shortlist features
 # If we do a naive brute force through all variables, we will have to deal with 2^23 combinations
 # which will take too much time. On the other hand, we can group features in the various groups
@@ -33,17 +27,30 @@ y_test = read_data(filepath="y_test.pkl", istarget=True)
 # PAY_AMTX: PAY_AMT1 - PAY_AMT6     (X18-X23)
 # Less is more!
 
-# List column indices to be used
-col_idxs = [1, 2, 3, 4, 6, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+col_idxs_list = [
+    [6],
+    [1, 6],
+    [0, 1, 2, 4, 6, 11, 12, 13, 14, 15, 16],
+    [0, 2, 4, 6, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+    [0, 1, 2, 4, 6, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+]
 
-# Train ML model
-x_train = x_train.iloc[:, col_idxs]
-x_test = x_test.iloc[:, col_idxs]
-cls = get_clf()
-cls.fit(x_train, y_train)
+for col_idxs in col_idxs_list:
+    print(f"col_idxs = {col_idxs}")
+    # Load data from pkl files
+    x_train = read_data(filepath="x_train.pkl", istarget=False)
+    y_train = read_data(filepath="y_train.pkl", istarget=True)
+    x_test = read_data(filepath="x_test.pkl", istarget=False)
+    y_test = read_data(filepath="y_test.pkl", istarget=True)
 
-# Evaluate ML model training and testing performance
-print(classification_report_table(cls, x_train, y_train, [0,1], \
-                                  "classification_report(x_train, y_train)"))
-print(classification_report_table(cls, x_test, y_test, [0,1], \
-                                  "classification_report(x_test, y_test)"))
+    # Train ML model
+    x_train = x_train.iloc[:, col_idxs]
+    x_test = x_test.iloc[:, col_idxs]
+    cls = get_clf()
+    cls.fit(x_train, y_train)
+
+    # Evaluate ML model training and testing performance
+    print(classification_report_table(cls, x_train, y_train, [0,1], \
+                                    "classification_report(x_train, y_train)"))
+    print(classification_report_table(cls, x_test, y_test, [0,1], \
+                                    "classification_report(x_test, y_test)"))
